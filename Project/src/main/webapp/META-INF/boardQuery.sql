16e08a71cc20ff4ac343a65b846ef2e3956b0ea9
HongSoonho <0113784358@naver.com>
2015-06-16 11:31:42 +0900

    게시판 쿼리

---
+++ Project/src/main/webapp/META-INF/boardQuery.sql
@@ -0,0 +1,133 @@
+
+Board
+BOARDCODE(FK)	게시판코드(FK)	NUMBER	NOT NULL
+BOARDNUM(PK)	게시글번호(PK)	NUMBER	NOT NULL
+BOARDNOTICE	공지여부	NUMBER	NULL
+USERID(FK)	아이디(FK)	VARCHAR2(50)	NOT NULL
+BOARDTITLE	글제목	VARCHAR2(100)	NOT NULL
+BOARDCONTENT	글내용	VARCHAR2(1000)	NOT NULL
+BOARDDATE	작성일	DATE	NOT NULL
+BOARDCOUNT	조회수	NUMBER	NOT NULL
+BOARDFILESRC	파일경로	VARCHAR2(100)	NULL
+BOARDREF	답글	NUMBER	NULL
+BOARDDEPTH	들여쓰기	NUMBER	NULL
+BOARDSTEP	답글계층	NUMBER	NULL
+
+NUM,    ref,  depth,  step 
+1        1      0       0                원본 
+2        1      1       4 +1 4           1번 답변
+3        1      1       3 +1 3           현재 1 step 0 보다 큰것 +1
+4        1      1       1
+5        5      0       0
+6        1      2       2                 4번 답변글에 대한 답변
+
+
+select * from board order by ref desc , step asc;
+
+--답글 : 최신 답변이 위로
+--화면출력
+  5번 ....
+  1번
+    4번
+      6번
+    3번
+    2번
+
+NUM,    ref,  depth,  step 
+1		1		0		0
+2		1		1		1+1+1
+3		1		1		1+1
+4		1		1		1
+5		5		0		0
+6		1		?		?		4번글의 대한 답변
+								4번글의 refer는 4가아닌 1이다 1번글의 답글이기때문이다. 그러기에 6번글의 refer값은 1을 따라간다. (원본글의 refer 값을 따라감.)
+								4번글의 depth는 1이다 그러기에 6번글의 depth값은 1에 +1 값엔 2가된다.						  (원본글의 depth값에 +1 하면된다.)
+								4번글의 step은 1이다. refer가 1인 글들의 step을 +1해주고 6번글은 원본글 step에 +1값을 넣어주면 된다.	(원본글의 step값에 +1 하면된다.)
+																													(원본글 refer와 같고 원본글의 step보다 큰 글들의 step 값을 +1해준다)
+
+NUM,    ref,  depth,  step 
+1		1		0		0
+2		1		1		3+1
+3		1		1		2+1
+4		1		1		1
+5		5		0		0
+6		1		2		(1+1)
+																													   
+하지만 이 로직을 따라갈 경우 3번글에 답글을 달경우 STEP은 어찌 될것인가?
+
+--답글 : 최신 답변이 위로
+--화면출력
+  5번 ....
+  1번
+    4번
+      6번
+    3번
+      7번
+    2번
+
+
+
+결국 답글에서 STEP 값은 원본글의 step값 이상값을 가진 글들의 step을 1씩 올리고
+새로 들어오는 답글이 원본글의 step값을 가진다
+
+BUT 
+원본글 STEP이 0인경우는 STEP값에 1이 들어간다.
+
+
+
+정리
+글쓰기
+
+NUM:  시퀀스 값 사용
+REFER: NVL(MAX(NUM),0)+1
+DEPTH: 기본값인 0
+STEP : 기본값인 0
+
+답글쓰기
+NUM:  시퀀스 값 사용
+REFER:원본글의 REFER 값을 따라간다.
+DEPTH:원본글의 DEPTH 값에 +1 
+STEP: 원본글의 STEP 값에 +1 해주면 된다
+	  단 원본글의 REFER 값과 같고 원본글의 STEP 값보다 큰 글들의 STEP 값을 +1 만큼 해준다
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+본격적인 SQL문 시작
+
+글쓰기
+
+
+Board
+BOARDCODE(FK)	게시판코드(FK)	NUMBER	NOT NULL
+BOARDNUM(PK)	게시글번호(PK)	NUMBER	NOT NULL
+BOARDNOTICE	공지여부	NUMBER	NULL
+USERID(FK)	아이디(FK)	VARCHAR2(50)	NOT NULL
+BOARDTITLE	글제목	VARCHAR2(100)	NOT NULL
+BOARDCONTENT	글내용	VARCHAR2(1000)	NOT NULL
+BOARDDATE	작성일	DATE	NOT NULL
+BOARDCOUNT	조회수	NUMBER	NOT NULL
+BOARDFILESRC	파일경로	VARCHAR2(100)	NULL
+BOARDREF	답글	NUMBER	NULL
+BOARDDEPTH	들여쓰기	NUMBER	NULL
+BOARDSTEP	답글계층	NUMBER	NULL
+
+
+select * from board order by ref desc , step asc;
+--보드리스트 기본적인 로직...
+
+insert into Board(BOARDCODE,BOARDNUM,BOARDNOTICE,USERID,BOARDTITLE,BOARDCONTENT,BOARDDATE,BOARDCOUNT,BOARDFILESRC,BOARDREF,BOARDDEPTH,BOARDSTEP)
+values(?,board_boardnum.nextval,?,?,?,?,?,?,?,?,?,?);
+--새글쓰기
+
+update Board set BOARDNOTICE=?, USERID=?, BOARDTITLE=?, BOARDCONTENT=?, BOARDFILESRC=? where BOARDNUM=?;
+--글 수정 하기
+
+update BOARD SET BOARDCOUNT=BOARDCOUNT+1 where BOARDNUM=?;
+--조회수 올리기
+
+delete from BOARD where BOARDNUM=?;
+--글 삭제하기.
+
+
+
+--답글쓰기에 필요한 쿼리문--
+select * from board
