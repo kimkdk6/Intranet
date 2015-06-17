50c71abd0b210690d0336df2c472dbb3b4fb33d0
DK <KOSTA@KOSTA-HP>
2015-06-17 18:24:24 +0900

    123

--- Project/src/main/java/controllers/Logincontroller.java
+++ Project/src/main/java/controllers/Logincontroller.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import dao.EmpDAO;
+import dao.MainDAO;
+import dto_vo.Board.Board;
 import dto_vo.Emp.Emp;
 
 @Controller
@@ -28,7 +31,7 @@ public class Logincontroller {
 	}
 	
 	@RequestMapping(value="index.htm", method=RequestMethod.POST)
-	public String login(Emp emp) throws ClassNotFoundException, SQLException 
+	public String login(Emp emp, Model model) throws ClassNotFoundException, SQLException 
 	{
 		/*
 		System.out.println("id : " + emp.getUserid());
@@ -44,7 +47,14 @@ public class Logincontroller {
 		
 		if( inputPwd.equals(emp.getEmppwd()))
 		{
-			// 메인 페이지 url
+			MainDAO recently = sqlSession.getMapper(MainDAO.class);
+			
+			List<Board> RecentlyNotice= recently.recentlyNotice();
+			model.addAttribute("RecentlyNotice", RecentlyNotice);
+			
+			List<Board> RecentlyBoard= recently.recentlyBoard();
+			model.addAttribute("RecentlyBoard", RecentlyBoard);
+
 			return "main.main";
 		} else {
 			return "login.login";
