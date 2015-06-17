363f4c95a8ae89bb4e50c4598966b8b1ab912134
DK <KOSTA@KOSTA-HP>
2015-06-17 16:30:39 +0900

    MainController

---
+++ Project/src/main/java/controllers/MainController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dao.IndexDAO;
+import dto_vo.Board.Board;
+
+@Controller
+@RequestMapping("/main/")
+public class MainController {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value = "main.htm")
+	public String index(Model model) throws ClassNotFoundException, SQLException
+	{
+		IndexDAO recentlyNotice = sqlSession.getMapper(IndexDAO.class);
+		List<Board> RecentlyNotice= recentlyNotice.recentlyNotice();
+		model.addAttribute("RecentlyNotice", RecentlyNotice);
+		return "main.index";
+
+	}
+}
