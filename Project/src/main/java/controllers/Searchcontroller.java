9b5a4e690643a0614b8ec0a4b4452b06d8a16fd4
KOSTA <KOSTA@KOSTA-HP>
2015-06-30 16:53:13 +0900

    검색컨트롤러

---
+++ Project/src/main/java/controllers/Searchcontroller.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import java.security.Principal;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dao.EmpDAO;
+
+import dto_vo.Emp.Emp;
+
+@Controller
+@RequestMapping("/search/")
+public class Searchcontroller {
+	@Autowired
+	private SqlSession sqlsession;
+	
+	@RequestMapping(value = "SearchEmp.htm")
+	public String SearchEmp(Model model, Principal principal) throws ClassNotFoundException, SQLException {
+		System.out.println("회원 검색 페이지 열람");
+		EmpDAO empdao = sqlsession.getMapper(EmpDAO.class);
+		
+		// String userid = principal.getName();
+		List<Emp> emplist = empdao.getEmplist();
+		
+		model.addAttribute("emplist", emplist);
+		
+		return "";
+		
+		
+	}
+}
