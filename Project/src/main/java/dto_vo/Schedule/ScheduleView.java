8fc1abedb80b9cdf35108fe81ad6365e74ca23d7
HongSoonho <0113784358@naver.com>
2015-06-30 15:06:40 +0900

    hgjhj

---
+++ Project/src/main/java/dto_vo/Schedule/ScheduleView.java
@@ -0,0 +1,116 @@
+package dto_vo.Schedule;
+
+import java.util.Date;
+
+// =====================스케쥴=========================
+public class ScheduleView {
+	private int schnum; // 스케쥴번호
+	private int catecode; // 카테고리코드
+	private String userid; // 사용자ID
+	private Date scstart; // 시작시간
+	private Date scend; // 종료시간
+	private String sctitle; // 스케쥴제목
+	private String sccontent; // 스케쥴내용
+	private String color;
+	private String catename;
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getCatename() {
+		return catename;
+	}
+
+	public void setCatename(String catename) {
+		this.catename = catename;
+	}
+
+	public ScheduleView() {}
+
+
+
+	public int getSchnum() {
+		return schnum;
+	}
+
+	public void setSchnum(int schnum) {
+		this.schnum = schnum;
+	}
+
+	public int getCatecode() {
+		return catecode;
+	}
+
+	public ScheduleView(int schnum, int catecode, String userid, Date scstart,
+			Date scend, String sctitle, String sccontent, String color,
+			String catename) {
+		super();
+		this.schnum = schnum;
+		this.catecode = catecode;
+		this.userid = userid;
+		this.scstart = scstart;
+		this.scend = scend;
+		this.sctitle = sctitle;
+		this.sccontent = sccontent;
+		this.color = color;
+		this.catename = catename;
+	}
+
+	public void setCatecode(int catecode) {
+		this.catecode = catecode;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public Date getScstart() {
+		return scstart;
+	}
+
+	public void setScstart(Date scstart) {
+		this.scstart = scstart;
+	}
+
+	public Date getScend() {
+		return scend;
+	}
+
+	public void setScend(Date scend) {
+		this.scend = scend;
+	}
+
+	public String getSctitle() {
+		return sctitle;
+	}
+
+	public void setSctitle(String sctitle) {
+		this.sctitle = sctitle;
+	}
+
+	public String getSccontent() {
+		return sccontent;
+	}
+
+	public void setSccontent(String sccontent) {
+		this.sccontent = sccontent;
+	}
+
+	@Override
+	public String toString() {
+		return "Schedule [schnum=" + schnum + ", catecode=" + catecode
+				+ ", userid=" + userid + ", scstart=" + scstart + ", scend="
+				+ scend + ", sctitle=" + sctitle + ", sccontent=" + sccontent
+				+ "]";
+	}
+	
+}
