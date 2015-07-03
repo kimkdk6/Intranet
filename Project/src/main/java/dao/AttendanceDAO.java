ea7e86428e0504faf4a097039b658eaca499ee89
DK <KOSTA@KOSTA-HP>
2015-07-03 11:06:55 +0900

    근태현황 피드백 요청합니다.

--- Project/src/main/java/dao/AttendanceDAO.java
+++ Project/src/main/java/dao/AttendanceDAO.java
@@ -17,20 +17,35 @@ public interface AttendanceDAO {
 	public String leavecheck(String userid) throws ClassNotFoundException, SQLException;
 	public void leavereason(String userid, String reason) throws ClassNotFoundException, SQLException;
 	public List<String> allmember(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	
 	public int offlateness(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int devellateness(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int manalateness(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int busilateness(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int alllateness(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	
 	public int offleave(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int develleave(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int manaleave(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int busileave(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int allleave(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	
 	public int offabsence(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int develabsence(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int manaabsence(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	public int busiabsence(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int allabsence(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	
+	public int offholiday(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int develholiday(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int manaholiday(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int busiholiday(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int allholiday(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	
+	public int offbiztrip(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int develbiztrip(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int manabiztrip(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int busibiztrip(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
+	public int allbiztrip(String yy, String mm, String dd) throws ClassNotFoundException, SQLException;
 	
 }
